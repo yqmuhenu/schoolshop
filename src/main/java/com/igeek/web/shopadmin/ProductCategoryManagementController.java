96e4282dd1bea06ebaa96da744342c39a6099d60
yqmuhenu <muyaqi.henu@foxmail.com>
2019-09-05 22:08:11 +0800

    完成店铺商品类别的后端查询功能

---
+++ src/main/java/com/igeek/web/shopadmin/ProductCategoryManagementController.java
@@ -0,0 +1,114 @@
+package com.igeek.web.shopadmin;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.igeek.dto.Result;
+import com.igeek.entity.ProductCategory;
+import com.igeek.entity.Shop;
+import com.igeek.enums.ProductCategoryStateEnum;
+import com.igeek.service.ProductCategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/productcategory")
+public class ProductCategoryManagementController {
+	@Autowired
+	private ProductCategoryService productCategoryService;
+
+	@RequestMapping(value = "/getproductcategorylist", method = RequestMethod.GET)
+	@ResponseBody
+	private Result<List<ProductCategory>> listProductCategorys(
+			HttpServletRequest request) {
+		/*Shop shop = new Shop();
+		shop.setShopId(1L);//测试使用
+		request.getSession().setAttribute("currentShop",shop);*/
+
+		Shop currentShop = (Shop) request.getSession().getAttribute(
+				"currentShop");
+		List<ProductCategory> list = null;
+		if (currentShop != null && currentShop.getShopId() > 0) {
+			list = productCategoryService.getProductCategoryList(currentShop.getShopId());
+			return new Result<List<ProductCategory>>(true, list);// WEB-INF/html/"shoplist".html
+		} else {
+			ProductCategoryStateEnum ps = ProductCategoryStateEnum.INNER_ERROR;//操作失败
+			return new Result<List<ProductCategory>>(false, ps.getState(),
+					ps.getStateInfo());
+		}
+	}
+
+	/*@RequestMapping(value = "/addproductcategorys", method = RequestMethod.POST)
+	@ResponseBody
+	private Map<String, Object> addProductCategorys(
+			@RequestBody List<ProductCategory> productCategoryList,
+			HttpServletRequest request) {
+		Map<String, Object> modelMap = new HashMap<String, Object>();
+		Shop currentShop = (Shop) request.getSession().getAttribute(
+				"currentShop");
+		for (ProductCategory pc : productCategoryList) {
+			pc.setShopId(currentShop.getShopId());
+		}
+		if (productCategoryList != null && productCategoryList.size() > 0) {
+			try {
+				ProductCategoryExecution pe = productCategoryService
+						.batchAddProductCategory(productCategoryList);
+				if (pe.getState() == ProductCategoryStateEnum.SUCCESS
+						.getState()) {
+					modelMap.put("success", true);
+				} else {
+					modelMap.put("success", false);
+					modelMap.put("errMsg", pe.getStateInfo());
+				}
+			} catch (RuntimeException e) {
+				modelMap.put("success", false);
+				modelMap.put("errMsg", e.toString());
+				return modelMap;
+			}
+
+		} else {
+			modelMap.put("success", false);
+			modelMap.put("errMsg", "请至少输入一个商品类别");
+		}
+		return modelMap;
+	}
+
+	@RequestMapping(value = "/removeproductcategory", method = RequestMethod.POST)
+	@ResponseBody
+	private Map<String, Object> removeProductCategory(Long productCategoryId,
+			HttpServletRequest request) {
+		Map<String, Object> modelMap = new HashMap<String, Object>();
+		if (productCategoryId != null && productCategoryId > 0) {
+			try {
+				Shop currentShop = (Shop) request.getSession().getAttribute(
+						"currentShop");
+				ProductCategoryExecution pe = productCategoryService
+						.deleteProductCategory(productCategoryId,
+								currentShop.getShopId());
+				if (pe.getState() == ProductCategoryStateEnum.SUCCESS
+						.getState()) {
+					modelMap.put("success", true);
+				} else {
+					modelMap.put("success", false);
+					modelMap.put("errMsg", pe.getStateInfo());
+				}
+			} catch (RuntimeException e) {
+				modelMap.put("success", false);
+				modelMap.put("errMsg", e.toString());
+				return modelMap;
+			}
+
+		} else {
+			modelMap.put("success", false);
+			modelMap.put("errMsg", "请至少选择一个商品类别");
+		}
+		return modelMap;
+	}*/
+
+}
